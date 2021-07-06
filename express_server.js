3aaad208b9050e2b779b1968d8db83f32312fb3e
vagrant <chiomaanierobi@gmail.com>
2021-07-06 17:01:06 +0000

    Add second route to the template

--- express_server.js
+++ express_server.js
@@ -2,22 +2,43 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");  // tells the Express app to use EJS as its templating engine. 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+//});
+
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+
+app.get('/urls/:shortURL', function (req, res) {
+  const shortURL = req.params.shortURL;
+  //console.log(urlDatabase[shortURL]);
+  const templateVars = {shortURL: shortURL, longURL: urlDatabase[shortURL] }
+  // console.log(req.params);
+  //console.log(urlDatabase);
+  //res.send(req.params);
+  res.render("urls_show", templateVars);
 });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 
 // Will a variable that is created in one request be accessible in another?
