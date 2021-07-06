3e24bbb394d839a6dbb54cfa5d0e6e460ad500fe
vagrant <chiomaanierobi@gmail.com>
2021-07-06 20:54:41 +0000

    Implement redirect after form submission

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // function to generate random words
 function generateRandomString() {
-  let key = Math.random().toString(36).substr(2,8);
+  let key = Math.random().toString(36).substr(2,6);
   return key;
 }
 
@@ -33,16 +33,27 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// Request a new url form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new");    // server finds the url_new template, generates the html and sends it back to the browser
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+
+  // Server generate a new shortURL and saves it to the urlDatabase.
+  let key = generateRandomString();
+  urlDatabase[key] = req.body.longURL;
+  console.log(urlDatabase);
+
+  // Redirect After Form Submission
+  res.redirect(`/urls/${key}`);
+
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
+/* Server looks up the longURL from the urlDatabase, passes the short and long URLs to the template,
+ generates the html then sends the html back to the browser*/
 app.get('/urls/:shortURL', function (req, res) {
   const shortURL = req.params.shortURL;
   //console.log(urlDatabase[shortURL]);
@@ -50,7 +61,7 @@ app.get('/urls/:shortURL', function (req, res) {
   // console.log(req.params);
   //console.log(urlDatabase);
   //res.send(req.params);
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars); // bowserr renders the html received from the server
 });
 
 
